79dc507450cb80e8aa5e7208dffd78a0afbdc3ba
RayceeM <mwatelaraycee33@gmail.com>
2018-03-29 12:38:45 +0300

    first screen added

---
+++ app/src/main/java/com/example/android/quizzapp/start_quiz.java
@@ -0,0 +1,13 @@
+package com.example.android.quizzapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class start_quiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_quiz);
+    }
+}
